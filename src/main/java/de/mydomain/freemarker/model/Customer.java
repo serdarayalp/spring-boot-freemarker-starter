5e40038a5c9edf145803257d230cb065a13395cc
Serdar Ayalp <serdar.ayalp@regiocom.com>
2023-06-19 15:43:05 +0200

    Codeformatierung

--- src/main/java/de/mydomain/freemarker/model/Customer.java
+++ src/main/java/de/mydomain/freemarker/model/Customer.java
@@ -1,10 +1,6 @@
 package de.mydomain.freemarker.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "customers")
